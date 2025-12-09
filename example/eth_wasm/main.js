2a5df3c8d859aa3d92cdb49c0a7c3a91d0150e97
Jingping Yin <yinjingping2022@gmail.com>
2025-12-09 09:50:13 +0800

    feat: upgrade ziren to v1.2.3

--- example/eth_wasm/main.js
+++ example/eth_wasm/main.js
@@ -10,8 +10,8 @@ import fs from 'node:fs'
 
 const vkey = fs.readFileSync('../binaries/eth_vk.bin');
 
-// Download the proof from https://ethproofs.org/blocks/23645600 > ZKM
-const proof = fs.readFileSync('../binaries/zkm_84a01f4b-8078-44cf-b463-90ddcd124960_812840.bin');
+// Download the proof from https://ethproofs.org/clusters/84a01f4b-8078-44cf-b463-90ddcd124960 > 23974000
+const proof = fs.readFileSync('../binaries/zkm_84a01f4b-8078-44cf-b463-90ddcd124960_2680786.bin');
 
 const startTime = performance.now();
 const result = wasm.verify_stark_proof(proof, vkey);
